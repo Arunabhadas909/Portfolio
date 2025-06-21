3178eb92c864fc16f5002636e29d1afc70e44a0a
Arunabha Das <arunabhadas909@gmail.com>
2025-06-21 22:07:36 +0530

    version-1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+    {path:'', redirectTo:'/userComp',pathMatch:'full'},
+    {path:'userComp', component:UserComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+    imports: [RouterModule.forRoot(routes, {
+
+    anchorScrolling: 'enabled',
+    scrollPositionRestoration : 'enabled',
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { UserIntroComponent } from './user/user-intro/user-intro.component';
+import { UserSkillsComponent } from './user/user-skills/user-skills.component';
+import { UserProjectsComponent } from './user/user-projects/user-projects.component';
+import { UserCoverletterComponent } from './user/user-coverletter/user-coverletter.component';
+import { UserFooterComponent } from './user/user-footer/user-footer.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,UserComponent,UserComponent,
+    UserIntroComponent,
+    UserSkillsComponent,
+    UserProjectsComponent,
+    UserCoverletterComponent,
+    UserFooterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,FormsModule,HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
